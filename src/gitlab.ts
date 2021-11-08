41a16fcb110a3834315b3ebae4a8d2119c20d1d7
webxmsj <17625492424@163.com>
2021-11-08 16:41:25 +0800

    添加点击通知打开mr合并页

--- src/gitlab.ts
+++ src/gitlab.ts
@@ -2,18 +2,18 @@ import CONFIG from './configuration'
 import { AxiosInstance } from './api'
 import { window, commands, Uri } from 'vscode'
 import { isArray } from './utils'
+import { AxiosError } from 'axios'
 
 export class GitLabMR {
   private gitlabUrl: string
   private privateToken: string
   private apiInstance: AxiosInstance
 
-  mergeRequests: object[] = []
+  mergeRequests: any[] = []
 
   constructor () {
     this.gitlabUrl = CONFIG.SETTING_CONFIG.gitlabUrl
     this.privateToken = CONFIG.SETTING_CONFIG.privateToken
-
     this.apiInstance = new AxiosInstance(this.gitlabUrl, this.privateToken)
   }
 
@@ -35,19 +35,19 @@ export class GitLabMR {
     return new Promise(async (resolve, reject) => {
       try {
         await this.getMRListAssignedToMe()
-  
         if (this.mergeRequests.length > 0) {
           const hasMergeRequestsTxt = `你的小弟们发给你${this.mergeRequests.length}个合并请求，快去合并代码取悦他们吧！`
           const openGitLabBtnTxt = '打开gitlab'
+          const openUrl = this.mergeRequests.length === 1 ? this.mergeRequests[0].web_url : this.gitlabUrl
           window.showInformationMessage(hasMergeRequestsTxt, openGitLabBtnTxt).then(selected => {
-            selected === openGitLabBtnTxt && commands.executeCommand('vscode.open', Uri.parse(CONFIG.SETTING_CONFIG.gitlabUrl))
+            selected === openGitLabBtnTxt && commands.executeCommand('vscode.open', Uri.parse(openUrl))
           })
         }
 
-        resolve()
+        resolve(this.mergeRequests)
       } catch (error) {
         // if the status exists
-        const { response } = error
+        const { response } = error as AxiosError
         // throw the response message
         if (response) {
           error = response.data.message
